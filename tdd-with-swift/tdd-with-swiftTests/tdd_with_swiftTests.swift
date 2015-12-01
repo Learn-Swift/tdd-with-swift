27ec18be89d42f0cccc192ab0a288e087b3a8b50
Duc Nguyen <nnduc@smartdev.vn>
2015-12-01 11:17:00 +0700

    initial

---
+++ tdd-with-swift/tdd-with-swiftTests/tdd_with_swiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  tdd_with_swiftTests.swift
+//  tdd-with-swiftTests
+//
+//  Created by Duc Nguyen on 12/1/15.
+//  Copyright © 2015 Duc Nguyen. All rights reserved.
+//
+
+import XCTest
+@testable import tdd_with_swift
+
+class tdd_with_swiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
